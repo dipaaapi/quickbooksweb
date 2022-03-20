b50e39b00cd4a52962d9549ce70f4048cb6bf3c6
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-03-20 11:30:38 +0800

    add inpuit function display on console log

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import { info } from "./Info";
 const App = () => {
 
     const [count, setCount] = useState(0)
+    const [user, setUser] = useState('')
 
     const adding = () => {
         setCount(count + 1)
+    }
+
+    const handleOnChange = (e) => {
+        setUser(e.target.value)
+        // console.log("e: ", e.target.value)
+        console.log("user: ", user)
     }
 
     return (
@@ -15,6 +22,8 @@ const App = () => {
             <br />
             <p>{count}</p>
             <button id="add" onClick={adding}>add</button>
+            <br />
+            <input name="user" onChange={handleOnChange} value={user} />
         </React.Fragment>
     )
 }
