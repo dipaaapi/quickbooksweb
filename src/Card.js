1c4195bdad6162d4587647f1827e721683630494
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-03-22 21:57:34 +0800

    update card component

--- src/Card.js
+++ src/Card.js
@@ -5,11 +5,23 @@ const Card = ({ name, age, gender, position }) => {
     // const { name, age, gender, position } = props
     return (
         <div className="card-main">
-            <h1>Users Card</h1>
-            <div>Name: {name}</div>
-            <div>Name: {position}</div>
-            <div>Age: {age}</div>
-            <div>Gender: {gender}</div>
+            <h1 className="title-main">Users Card</h1>
+            <div className="card-data">
+                <p className="data-inner-left">Name: </p>
+                <p className="data-inner-right">{name}</p>
+            </div>
+            <div className="card-data">
+                <p className="data-inner-left">Position: </p>
+                <p className="data-inner-right">{position}</p>
+            </div>
+            <div className="card-data">
+                <p className="data-inner-left">Age: </p>
+                <p className="data-inner-right">{age}</p>
+            </div>
+            <div className="card-data">
+                <p className="data-inner-left">Gender: </p>
+                <p className="data-inner-right">{gender}</p>
+            </div>
         </div>
     )
 }
